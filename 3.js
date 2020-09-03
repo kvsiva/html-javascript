08fd59e93cd88ea8a53aafbc2206e8bdd425f214
kvsiva <sivakrishna.konijeti@gmail.com>
2020-09-03 20:10:00 +0530

    fes js are added

---
+++ 3.js
@@ -0,0 +1,5 @@
+<script>
+    var x = new Date("Sept 03,2020")
+    var currentDate=x.parse();
+    console.log(currentDate)
+</script>
---
+++ 4.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    var numberString ="22";
+    var response =parseInt(numberString)
+    if(numberString == response) {
+        console.log("Inside 1st if condition")
+    }
+    if(numberString === response){
+        console.log("Inside 2nd if condition")
+    }
+</script>
